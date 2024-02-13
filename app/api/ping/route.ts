0b5402b3a71cb789b87f27163c6ecb01145f2f65
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-02-13 23:40:13 +0530

    fix: db schema
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,4 +1,9 @@
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return new Response('pong! welcome to dresume.me')
+    return NextResponse.json({
+        "message": 'pong! welcome to dresume.me',
+        "status": 200,
+        "data": null,
+    })
 }
